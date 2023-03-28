15726c3f1f7c003d446c4d1e9d3ca600db926d6c
Juan Torres <juane@backbase.com>
2023-03-28 12:14:08 -0500

    Class 21: Login with third parties and JWT. Updated class 20 bugfix.

--- module2/clase21-third-party-auth/src/config/passport.config.js
+++ module2/clase21-third-party-auth/src/config/passport.config.js
@@ -1,16 +1,50 @@
 import passport from 'passport';
 import passportLocal from 'passport-local';
+import GitHubStrategy from 'passport-github2';
 import userModel from '../models/user.model.js';
-import {createHash, isValidPassword} from '../utils.js';
 
 //Declaramos nuestra estrategia:
 const localStrategy = passportLocal.Strategy;
-
 const initializePassport = () => {
    /**
-     *  Inicializando la estrategia local, username sera para nosotros email.
+     *  Inicializando la estrategia para github.
      *  Done será nuestro callback
     */ 
+    //Estrategia de Login con GitHub:
+    passport.use('github', new GitHubStrategy(
+        {
+            clientID: 'Iv1.a24db95353dfb021', 
+            clientSecret: '2f6fdf5ad4c7331b19ae7a4d75f2a4891aa0e697',
+            callbackUrl: 'http://localhost:9090/api/sessions/githubcallback'
+        }, 
+        async (accessToken, refreshToken, profile, done) => {
+            console.log("Profile obtenido del usuario: ");
+            console.log(profile);
+            try {
+                const user = await userModel.findOne({email: profile._json.email});
+                console.log("Usuario encontrado para login:");
+                console.log(user);
+                if (!user) {
+                    console.warn("User doesn't exists with username: " + profile._json.email);
+                    let newUser = {
+                        first_name: profile._json.name,
+                        last_name: '',
+                        age: 18,
+                        email: profile._json.email,
+                        passport: '',
+                    };
+                    const result = await userModel.create(newUser);
+                    return done(null, result);
+                } else {
+                    //Si entramos por acá significa que el usuario ya existía.
+                    return done(null, user);
+                }
+            } catch (error) {
+                return done(error);
+            }
+        })
+    );
+    //Estrategia de registro de usuario
     passport.use('register', new localStrategy(
         {passReqToCallback: true, usernameField: 'email'}, async (req, username, password, done) => {
             const {first_name, last_name, email, age} = req.body;
@@ -36,7 +70,7 @@ const initializePassport = () => {
         }
     ));
     
-    //Estrategia de Login:
+    //Estrategia de Login de la app:
     passport.use('login', new localStrategy(
         {passReqToCallback: true, usernameField: 'email'}, async (req, username, password, done) => {
             try {
--- module2/clase21-third-party-auth/src/routes/sessions.router.js
+++ module2/clase21-third-party-auth/src/routes/sessions.router.js
@@ -3,6 +3,19 @@ import passport from 'passport';
 
 const router = Router();
 
+router.get("/github", passport.authenticate('github', {scope: ['user:email']}), async (req, res) => {});
+
+router.get("/githubcallback", passport.authenticate('github', {failureRedirect: '/github/error'}), async (req, res) => {
+    const user = req.user;
+    req.session.user= {
+        name : `${user.first_name} ${user.last_name}`,
+        email: user.email,
+        age: user.age
+    };
+    req.session.admin = true;
+    res.redirect("/github");
+});
+
 router.post("/register", passport.authenticate(
     'register', {failureRedirect: '/api/sessions/fail-register'})
     , async (req, res)=>{
@@ -19,7 +32,8 @@ router.post("/login", passport.authenticate('login', {failureRedirect: '/api/ses
         name : `${user.first_name} ${user.last_name}`,
         email: user.email,
         age: user.age
-    }
+    };
+    req.session.admin = true;
     res.send({status:"success", payload:req.session.user, message:"¡Primer logueo realizado! :)" });
 });
 
