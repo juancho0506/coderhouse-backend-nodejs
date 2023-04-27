a5a68c663f9a927a91369c5a92e32af4315a79f9
Juan Torres <juane@backbase.com>
2023-04-27 16:37:07 -0500

    Added template and handlebars implementation to render json

--- module2/clase29-server-with-layers-from-scratch/src/controllers/users.controller.js
+++ module2/clase29-server-with-layers-from-scratch/src/controllers/users.controller.js
@@ -1,6 +1,15 @@
+import UserService from '../services/dao/users.dao.js';
+
+const userService = new UserService();
 
 export const getUsers = async (req, res) => {
-    res.send({message: "Success!", payload: "getUsers: Por implementar"});
+    try {
+        let users = await userService.getAll();
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({error:  error, message: "No se pudo obtener los usuarios."});
+    }
 }
 
 export const getUserById = async (req, res) => {
@@ -8,5 +17,11 @@ export const getUserById = async (req, res) => {
 }
 
 export const saveUser = async (req, res) => {
-    res.send({message: "Success!", payload: "saveUser: Por implementar"});
+    try {
+        let result = await userService.save(req.body);
+        res.status(201).send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({error:  error, message: "No se pudo guardar el usuario."});
+    }
 }
