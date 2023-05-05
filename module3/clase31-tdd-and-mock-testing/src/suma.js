a0e38e542064ed5b442269e9a53d204e9925d753
Juan Torres <juane@backbase.com>
2023-05-04 22:33:23 -0500

    Refactored and optimized sum method.

--- module3/clase31-tdd-and-mock-testing/src/suma.js
+++ module3/clase31-tdd-and-mock-testing/src/suma.js
@@ -1,23 +1,11 @@
+//Suma Refactored and optimized with Functional Programming.
 export default (...numbers) => {
     console.log("Entrando a la suma con arreglo de numeros: ");
     console.log(numbers);
     if (numbers.length === 0) return 0;
-    let validInputs = true;
-    let i = 0;
-    while (validInputs && i<numbers.length) {
-        if (typeof numbers[i] !== "number") {
-            validInputs = false;
-            return null;
-        }
-        i++;
-    } 
-    console.log("Valid inputs? :" + validInputs);
-    if (!validInputs) return null;
+    if (!numbers.every(num => typeof num === "number")) return null;
     let result = 0;
-    
-    for (let i = 0; i < numbers.length; i++) {
-        result += numbers[i];
-    }
+    result = numbers.reduce((prev, current) => prev + current);
     console.log("Resultado de la suma:");
     console.log(result);
     return result;
