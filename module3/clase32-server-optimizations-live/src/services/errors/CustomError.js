dbe3223a75fa1154b8fbc36b877eb3ba9ac97eee
Juan Torres <juane@backbase.com>
2023-05-10 10:09:45 -0500

    Fixed Error message with cause, only on node v16.x

--- module3/clase32-server-optimizations-live/src/services/errors/CustomError.js
+++ module3/clase32-server-optimizations-live/src/services/errors/CustomError.js
@@ -1,6 +1,6 @@
 export default class CustomError {
     static createError({name="Error", cause, message, code=1}) {
-        const error = new Error(message, {cause});
+        const error = new Error(message, {cause: new Error(cause)});
         error.name= name;
         error.code= code;
         throw error;
