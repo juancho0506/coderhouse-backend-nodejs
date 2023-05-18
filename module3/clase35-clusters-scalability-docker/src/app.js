32cdb88ea56a9061e65aa6bd80e60a55690b0a7c
Juan Torres <juane@backbase.com>
2023-05-17 19:16:28 -0500

    Class35: Initial commit

---
+++ module3/clase35-clusters-scalability-docker/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import config from './config/config.js';
+//import Routers
+//Performance test:
+import performanceRouter from './routers/performance-test.router.js';
+import { addLogger } from './config/logger.js';
+
+const app = express();
+
+//JSON settings:
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(addLogger);
+
+
+//Declare routers:
+app.use("/api/performance", performanceRouter);
+
+const SERVER_PORT = config.port;
+app.listen(SERVER_PORT, () => {
+    console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
+});
