f23ca3adfeaf3b663bb17fe01fdb74aa3133e65a
Juan Torres <juane@backbase.com>
2023-06-14 22:37:13 -0500

    Clase 41 Supertest

--- module3/clase39-40-41-API-doc-and-testing-adoptme/src/controllers/pets.controller.js
+++ module3/clase39-40-41-API-doc-and-testing-adoptme/src/controllers/pets.controller.js
@@ -12,7 +12,7 @@ const createPet = async(req,res)=> {
     if(!name||!specie||!birthDate) return res.status(400).send({status:"error",error:"Incomplete values"})
     const pet = PetDTO.getPetInputFrom({name,specie,birthDate});
     const result = await petsService.create(pet);
-    res.send({status:"success",payload:result})
+    res.status(201).send({status:"success",payload:result})
 }
 
 const updatePet = async(req,res) =>{
